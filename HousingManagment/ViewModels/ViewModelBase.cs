5e445754dc31aa79af95e3637d7312344307d2bd
ShadeRey <andryusha_shadrin@bk.ru>
2023-11-21 13:35:12 +0700

    Initial commit

---
+++ HousingManagment/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace HousingManagment.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ HousingManagment/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace HousingManagment.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
